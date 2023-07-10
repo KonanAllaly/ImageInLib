0b3ef6df75c3f986d3a2b44ba5b1d566f604c445
Konan Allaly <konan.allaly@tatramed.sk>
2023-07-10 17:19:42 +0200

    add function for store 2d .raw

--- ImageInLib/ImageInLib/src/data_storage.h
+++ ImageInLib/ImageInLib/src/data_storage.h
@@ -37,6 +37,8 @@ extern "C" {
 		const size_t imageHeight, unsigned char * pathPtr, VTK_Header_Lines *lines);
 
 	bool store2dPGM(dataType** imageDataPtr, const size_t xDim, const size_t yDim, const char* pathPtr, const bool writeRawData);
+
+	bool store2dRawData(dataType* array2DPtr, const size_t xDim, const size_t yDim, const char* pathPtr, Storage_Flags flags);
 #ifdef __cplusplus
 }
 #endif
