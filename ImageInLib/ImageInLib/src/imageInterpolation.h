5eb4ce899c5babce25c4877dabaf193fe55c4005
Konan Allaly <konan.allaly@tatramed.sk>
2023-07-17 12:51:09 +0200

    ...

--- ImageInLib/ImageInLib/src/imageInterpolation.h
+++ ImageInLib/ImageInLib/src/imageInterpolation.h
@@ -21,15 +21,6 @@ extern "C" {
 
 	//=========================
 
-	//Structure to handle image spacing
-	typedef struct {
-		dataType sx, sy, sz;
-	} VoxelSpacing;
-
-	typedef struct {
-		Point3D v1, v2, v3;
-	}OrientationMatrix;
-
 	/*
 	* srcPoint : source 3d points in image coordinates system
 	* ImageOrigin : image origin in real world (ranslation)
@@ -40,18 +31,52 @@ extern "C" {
 	*/
 	Point3D imageCoordToRealCoord(Point3D srcPoint, Point3D realOrigin, VoxelSpacing imageSpacing, OrientationMatrix orientation);
 
-	//Get image coordinate from real coordinate
-	//Point3D realCoordToImageCoord(Point3D srcPoint, Point3D realOrigin, voxelSpacing3D imageSpacing, orientationMatrix orientation);
-
-	typedef struct {
-		dataType sx, sy;
-	} PixelSpacing;
+	/*
+	* srcPoint : source 3d points in real world coordinates system
+	* ImageOrigin : image origin in image coordinate system
+	* VoxelSpacing : - distance between voxels in x and y direction
+	*                - distance between slices (scaling)
+	* orientation : matrix for orientation/rotation IJK((1, 0, 0),(0,1,0),(0,0,1)), RAS((-1,0,0),(0,-1,0),(0,0,1))
+	* or LPS((0,0,1),(0,-1,0),(0,0,-))
+	*/
+	Point3D realCoordToImageCoord(Point3D srcPoint, Point3D realOrigin, VoxelSpacing imageSpacing, OrientationMatrix orientation);
 
 	/*
 	* oldImage to handle the input image (height, width and pointer for pixel value)
 	* newImage to handle the result image (height, width and pointer for pixel value)
 	*/
 	bool resizeImage(Image_Data2D oldImage, Image_Data2D newImage);
+
+	//=====================================================
+
+	/*
+	 * Get 2D point in real world Cordinates System from 2D image Coordinates System
+	 * Three operations are done here : scaling, translation and rotation
+	*/
+	Point2D getRealCoordFromImageCoord2D(Point2D srcPoint, Point2D realOrigin, PixelSpacing imageSpacing, OrientationMatrix2D orientation);
+
+	/*
+	* Get 2D point in image Coordinates System from 2D point in real Coordinates System
+	* Three operations are done here : scaling, translation and rotation
+	*/
+	Point2D getImageCoordFromRealCoord2D(Point2D srcPoint, Point2D imageOrigin, PixelSpacing imageSpacing, OrientationMatrix2D orientation);
+
+	/*
+	* This function perform image interpolation from image coordinates system to real world coordinate system
+	* resizeImageFromImageCoordToRealCoord(Image_Data2D src_image, Image_Data2D dest_image, OrientationMatrix2D orientation)
+	* Image_Data2D src_image : structure to handle image in "IMAGE Cordinates System"
+	*                          - height, width : image dimension
+	*                          - imageDataPtr : pointer for pixels value
+	*                          - origin : image origin
+	*                          - spacing : pixel size
+	* Image_Data2D dest_image : structure to handle interpolated image in "Real World Coordinates System"
+	*                          - height, width : interpolated image dimension
+	*                          - imageDataPtr : pointer for interpolated pixels value
+	*                          - origin : interpolated image origin
+	*                          - spacing : interpolated pixel size
+	* OrientationMatrix2D orientation : orientation of the rotation
+	*/
+	bool resizeImageFromImageCoordToRealCoord(Image_Data2D src_image, Image_Data2D dest_image, OrientationMatrix2D orientation);
 
 #ifdef __cplusplus
 }
