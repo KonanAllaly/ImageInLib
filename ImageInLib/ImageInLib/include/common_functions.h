c3db6a7234bd86cc6347309308f5565ae970363c
Konan Allaly <konan.allaly@tatramed.sk>
2023-08-31 17:51:53 +0200

    add structure for statistics

--- ImageInLib/ImageInLib/include/common_functions.h
+++ ImageInLib/ImageInLib/include/common_functions.h
@@ -73,9 +73,6 @@ extern "C" {
 		size_t xd, p;
 	}Random2dPoints;
 
-	//==============================================================================
-	// Structures for interpolation
-
 	typedef struct {
 		Point2D v1, v2;
 	}OrientationMatrix2D;
@@ -92,6 +89,10 @@ extern "C" {
 		PixelSpacing spacing; // pixel size
 		OrientationMatrix2D orientation;
 	} Image_Data2D;
+
+	typedef struct {
+		dataType min_data, max_data, mean_data, sd_data;
+	} Statistics;
 
 	//==============================================================================
 	// Shapes Container
@@ -182,6 +183,7 @@ extern "C" {
 	double getPoint3DDistance(const Point3D a, const Point3D b);
 	//==============================================================================
 	Point3D getPointWithTheHighestValue(dataType** distanceMapPtr, const size_t length, const size_t width, const size_t height);
+	//==============================================================================
 
 #endif // !COMMON_FUNCTIONS
 
