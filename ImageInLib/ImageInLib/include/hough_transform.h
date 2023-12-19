fbcadb0ec8279465eb2fe02048d87e722b7347a1
Konan Allaly <konan.allaly@tatramed.sk>
2023-12-19 11:12:24 +0100

    add function description

--- ImageInLib/ImageInLib/include/hough_transform.h
+++ ImageInLib/ImageInLib/include/hough_transform.h
@@ -13,10 +13,44 @@ typedef struct {
 	dataType radius_step; //step for changing the radius
 } HoughParameters;
 
+
+/// <summary>
+/// Initialize 2D array with 0.0
+/// </summary>
+/// <param name="array2D">2D array</param>
+/// <param name="length">array length</param>
+/// <param name="width">array width</param>
 void initialize2dArray(dataType* array2D, const size_t length, const size_t width);
 
+/// <summary>
+/// Get the coordinates of the pixel with the maximal value
+/// </summary>
+/// <param name="arrayPtr2D">2D array</param>
+/// <param name="length">array length</param>
+/// <param name="width">array width</param>
+/// <returns></returns>
 Point2D getPointWithMaximalValue2D(dataType* arrayPtr2D, const size_t length, const size_t width);
 
+/// <summary>
+/// Detect circles in given image
+/// </summary>
+/// <param name="imageDataPtr">input image data</param>
+/// <param name="houghSpacePtr">voting array for hough transform</param>
+/// <param name="foundCirclePtr">found circles</param>
+/// <param name="length">image length</param>
+/// <param name="width">image width</param>
+/// <param name="params">hough transform parameters</param>
 void circularHoughTransform(dataType* imageDataPtr, dataType* houghSpacePtr, dataType* foundCirclePtr, const size_t length, const size_t width, HoughParameters params);
 
+/// <summary>
+/// Detect cicles around given point
+/// </summary>
+/// <param name="seed">given point</param>
+/// <param name="imageDataPtr">input image data</param>
+/// <param name="houghSpacePtr">voting array for hough transform</param>
+/// <param name="foundCirclePtr">found circles</param>
+/// <param name="length">image length</param>
+/// <param name="width">image width</param>
+/// <param name="params">Hough transform parameters</param>
+/// <param name="savingPath">saving path</param>
 void localHoughTransform(Point2D seed, dataType* imageDataPtr, dataType* houghSpacePtr, dataType* foundCirclePtr, const size_t length, const size_t width, HoughParameters params, std::string savingPath);
