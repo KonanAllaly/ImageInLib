53db9f5368d7aee574a87b418b95afff8f114567
Konan Allaly <konan.allaly@tatramed.sk>
2024-09-25 16:29:53 +0200

    Add files for 3D Lagrangean segmentation

---
+++ ImageInLib/ImageInLib/src/segmentation3D_lagrangean.h
@@ -0,0 +1,13 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#pragma once
+#include <stdbool.h>
+#include <stddef.h>
+#include "common_functions.h"
+#include "segmentation.h"
+
+#ifdef __cplusplus
+}
+#endif
