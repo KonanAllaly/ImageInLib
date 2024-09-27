11b168c42e17053cc546feada2856074389ce3cf
Konan Allaly <konan.allaly@tatramed.sk>
2024-09-27 15:26:15 +0200

    betterr naming

--- ImageInLib/ImageInLib/include/segmentation.h
+++ ImageInLib/ImageInLib/include/segmentation.h
@@ -32,7 +32,7 @@ extern "C" {
 		dataType omega;	//redistribution speed
 		dataType edgeCoef; // edge detector coefficient 
 		dataType intensityCoef; // coeficient for the similar intensity detector
-		dataType refence_intensity; //The reference intensity for G2
+		dataType reference_intensity; //The reference intensity for G2
 		void(*get_velocity)(Image_Data2D*, double, double, double*, double*);//pointer to the function returning the velocity for a given coordinate
 		void(*get_g2)(Image_Data2D*, double, double, double, double, double*);//pointer to the function returning the g2 value for a given coordinate
 		bool open_curve;
