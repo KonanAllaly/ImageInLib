7302fab19c06da32e7622867e011f320a3e31175
Konan Allaly <konan.allaly@tatramed.sk>
2025-03-24 09:08:17 +0100

    reduce function parameters

--- ImageInLib/ImageInLib/src/segmentation.c
+++ ImageInLib/ImageInLib/src/segmentation.c
@@ -2,7 +2,6 @@
 #include "segmentation3D_subsurf.h"
 #include "segmentation3d_gsubsurf.h"
 #include "segmentation3D_atlas.h"
-#include "segmentation3d_gsubsurf.h"
 
 void segmentImage(Image_Data inputImageData, dataType** initialSegment, Segmentation_Parameters segParameters, Filter_Parameters explicit_lhe_Parameters,
 	Point3D* centers, size_t no_of_centers, unsigned char* outputPathPtr, const SegmentationMethod model)
@@ -13,7 +12,7 @@ void segmentImage(Image_Data inputImageData, dataType** initialSegment, Segmenta
 		subsurfSegmentation(inputImageData, initialSegment, segParameters, explicit_lhe_Parameters, centers, no_of_centers, outputPathPtr);
 		break;
 	case GSUBSURF_MODEL:
-		generalizedSubsurfSegmentation(inputImageData, initialSegment, segParameters, explicit_lhe_Parameters, centers, no_of_centers, outputPathPtr);
+		generalizedSubsurfSegmentation(inputImageData, initialSegment, segParameters, explicit_lhe_Parameters, outputPathPtr);
 		break;
 	default:
 		break;
