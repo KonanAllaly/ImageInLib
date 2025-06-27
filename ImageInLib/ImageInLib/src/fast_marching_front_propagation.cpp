b880724021799c4e84d7847bee9f61822a29fe73
Konan Allaly <konan.allaly@tatramed.sk>
2025-06-27 16:48:03 +0200

    add function for 3D front propagation

--- ImageInLib/ImageInLib/src/fast_marching_front_propagation.cpp
+++ ImageInLib/ImageInLib/src/fast_marching_front_propagation.cpp
@@ -25,13 +25,14 @@ void fastMarchingfrontPropagation2D(Image_Data2D inputImageData, dataType* actio
 	}
 }
 
-void fastMarchingfrontPropagation(Image_Data inputImageData, dataType** potential, Point3D* endPoints, const PropagationType pType)
+void fastMarchingfrontPropagation(Image_Data inputImageData, dataType** actionMapPtr, dataType** potentialPtr, Point3D* endPoints, const PropagationType pType)
 {
 	switch (pType)
 	{
-		/*	case PARTIAL_FRONT_PROPAGATION:
-			partialFrontPropagation(inputImageData, potential, endPoints);
+		case PARTIAL_FRONT_PROPAGATION:
+			partialFrontPropagation(inputImageData, actionMapPtr, potentialPtr, endPoints);
 			break;
+			/*
 		case FRONT_PROPAGATION:
 			frontPropagation(inputImageData, potential, endPoints);
 			break;
