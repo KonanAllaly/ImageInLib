b5b3754407fc7d4548167a3cdf2733c85ba9d2be
Konan Allaly <konan.allaly@tatramed.sk>
2025-07-04 17:42:01 +0200

    add structures and function for partial front propagation

--- ImageInLib/ImageInLib/include/fast_marching_front_propagation_2d.h
+++ ImageInLib/ImageInLib/include/fast_marching_front_propagation_2d.h
@@ -9,7 +9,30 @@ extern "C" {
 #include <stdbool.h>
 #include "common_functions.h"
 
+	typedef struct {
+		size_t x, y, z;
+		dataType arrival;
+	}pointFastMarching;
 
+	typedef struct {
+		dataType K; //edge detection coef
+		dataType thres;//edge detector threshold
+		dataType eps; //path smothing parameter
+		double radius;
+	} Potential_Parameters;
+
+	typedef enum
+	{
+		PARTIAL_FRONT_PROPAGATION = 1,
+		FRONT_PROPAGATION = 2,
+		DOUBLE_FRONT_PROPAGATION = 3,
+		KEY_POINT_DETECTION = 4,
+		DISTANCE_MAP = 5
+	} PropagationType;
+
+	void fastMarchingFrontPropagation(void* inputImageData, void* actionPtr, void* potential, void* endPoints, const PropagationType pType);
+
+	bool partialFrontPropagation2D(Image_Data2D inputImage, dataType* action, dataType* potential, Point2D* endPoint);
 
 #endif // !FAST_MARCHING_FRONT_PROPAGATION_2D
 
--- ImageInLib/ImageInLib/src/fast_marching_front_propagation_2d.c
+++ ImageInLib/ImageInLib/src/fast_marching_front_propagation_2d.c
@@ -1,1 +1,29 @@
 #include "fast_marching_front_propagation_2d.h"
+
+void fastMarchingFrontPropagation(void* inputImageData, void* actionPtr, void* potentialPtr, void* endPoints, const PropagationType pType)
+{
+	switch (pType)
+	{
+	case PARTIAL_FRONT_PROPAGATION:
+	{
+		Image_Data2D inputImage = *(Image_Data2D*)inputImageData;
+		dataType* action = (dataType*)actionPtr;
+		dataType* potential = (dataType*)potentialPtr;
+		Point2D* endPoint = (Point2D*)endPoints;
+		partialFrontPropagation2D(inputImage, action, potential, endPoint);
+		break;
+	}
+	default:
+		break;
+	}
+}
+
+bool partialFrontPropagation2D(Image_Data2D inputImage, dataType* action, dataType* potential, Point2D* endPoint)
+{
+	if(inputImage.imageDataPtr == NULL || action == NULL || potential == NULL || endPoint == NULL)
+	{
+		return false;
+	}
+
+	return true;
+}
