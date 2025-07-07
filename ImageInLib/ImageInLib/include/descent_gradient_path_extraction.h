accf209f46a2748a7eb3ee709485633f5d3d22cf
Konan Allaly <konan.allaly@tatramed.sk>
2025-07-07 17:43:25 +0200

    files and function for path extraction

--- ImageInLib/ImageInLib/include/descent_gradient_path_extraction.h
+++ ImageInLib/ImageInLib/include/descent_gradient_path_extraction.h
@@ -8,6 +8,14 @@ extern "C" {
 
 #include "common_functions.h"
 
+	typedef struct {
+		dataType tau; // used in the descent gradient
+		size_t max_iteration; // maximal iteration to stop descent gradient
+		dataType tolerance; //minimal distance to stop
+	} Path_Parameters;
+
+	bool shortestPath2d(Image_Data2D actionMapStr, Point2D* seedPoints, Path_Parameters parameters, unsigned char* pathPtr);
+
 
 #endif // !DESCENT_GRADIENT_PATH_EXTRACTION
 
--- ImageInLib/ImageInLib/src/descent_gradient_path_extraction.C
+++ ImageInLib/ImageInLib/src/descent_gradient_path_extraction.C
@@ -1,8 +1,9 @@
+#include <stdio.h>
+#include "math.h"
 #include "descent_gradient_path_extraction.h"
 
-bool shortestPath2d(Image_Data2D actionMapStr, Point2D* seedPoints, std::vector<Point2D>& path_points, Path_Parameters parameters) {
+bool shortestPath2d(Image_Data2D actionMapStr, Point2D* seedPoints, Path_Parameters parameters, unsigned char* pathPtr) {
 
-	/*
 	if (actionMapStr.imageDataPtr == NULL || seedPoints == NULL)
 		return false;
 
@@ -19,8 +20,15 @@ bool shortestPath2d(Image_Data2D actionMapStr, Point2D* seedPoints, std::vector<
 	dataType tau = parameters.tau;
 	size_t count_iter = 0;
 
+	FILE* saving_file;
+	if ((fopen_s(&saving_file, pathPtr, "w")) != 0)
+	{
+		printf("unable to open the file");
+		return false;
+	}
+	fprintf(saving_file, "x,y\n");
+	
 	do {
-
 		getGradient2D(actionMapStr.imageDataPtr, actionMapStr.height, actionMapStr.width, i, j, spacing, &grad);
 		dataType gradNorm = sqrt(grad.x * grad.x + grad.y * grad.y);
 		if (gradNorm < 1e-6) {
@@ -29,17 +37,17 @@ bool shortestPath2d(Image_Data2D actionMapStr, Point2D* seedPoints, std::vector<
 		}
 		x -= tau * grad.x / gradNorm;
 		y -= tau * grad.y / gradNorm;
+		fprintf(saving_file, "%f,%f\n", x, y);
 
 		Point2D current = { x, y };
 		dist_min = getPoint2DDistance(current, seedPoints[0]);
-		path_points.push_back(current);
 
 		i = (size_t)round(x);
 		j = (size_t)round(y);
 
 		count_iter++;
 	} while (dist_min > parameters.tolerance && count_iter < parameters.max_iteration);
-	*/
+	fclose(saving_file);
 
 	return true;
 }
