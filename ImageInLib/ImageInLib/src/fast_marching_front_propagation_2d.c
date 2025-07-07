bdcc563a4c7655e453c7045678a0cc7367f8e6dd
Konan Allaly <konan.allaly@tatramed.sk>
2025-07-07 12:22:18 +0200

    add function to solve the 2D quadratic eikonal equation

--- ImageInLib/ImageInLib/src/fast_marching_front_propagation_2d.c
+++ ImageInLib/ImageInLib/src/fast_marching_front_propagation_2d.c
@@ -2,6 +2,87 @@
 #include <math.h>
 #include "fast_marching_front_propagation_2d.h"
 
+dataType solve2dQuadratic(dataType dx, dataType dy, dataType p, PixelSpacing h) {
+
+	dataType solution = 0.0, a = 0.0, b = 0.0, c = 0.0, delta = 0.0;
+	dataType p_2 = p * p;
+
+	dataType hx = h.sx;
+	dataType hx_2 = h.sx * h.sx;
+
+	dataType hy = h.sy;
+	dataType hy_2 = h.sy * h.sy;
+
+	if (p <= 0.0) {
+		printf("Error: P must be positive.");
+		return INFINITY; // Return 0 if P is not positive
+	}
+
+	if (dx == INFINITY && dy != INFINITY)
+	{
+		a = 1.0;
+		b = -2 * dy;
+		c = (dataType)(dy * dy - hy_2 * p_2);
+		delta = (dataType)(b * b - 4 * a * c);
+		if (delta >= 0) {
+			solution = (dataType)((-b + sqrt(delta)) / (2 * a));
+			if (solution >= dy) {
+				return solution;
+			}
+			else {
+				return (dataType)(dy + hy * p);
+			}
+		}
+		else {
+			return (dataType)(dy + hy * p);
+		}
+	}
+
+	if (dx != INFINITY && dy == INFINITY)
+	{
+		a = 1.0;
+		b = -2 * dx;
+		c = (dataType)(dx * dx - hx_2 * p_2);
+		delta = (dataType)(b * b - 4 * a * c);
+		if (delta >= 0)
+		{
+			solution = (dataType)((-b + sqrt(delta)) / (2 * a));
+			if (solution >= dx)
+			{
+				return solution;
+			}
+			else {
+				return (dataType)(dx + hx * p);
+			}
+		}
+		else {
+			return (dataType)(dx + hx * p);
+		}
+	}
+
+	if (dx != INFINITY && dy != INFINITY)
+	{
+		a = hx_2 + hy_2;
+		b = -2 * (hy_2 * dx + hx_2 * dy);
+		c = (dataType)(hy_2 * dx * dx + hx_2 * dy * dy - hx_2 * hy_2 * p_2);
+		delta = (dataType)(b * b - 4 * a * c);
+		if (delta >= 0)
+		{
+			solution = (dataType)((-b + sqrt(delta)) / (2 * a));
+			if (solution >= fmax(dx, dy)) {
+				return solution;
+			}
+			else {
+				return (dataType)(fmin(dx + hx * p, dy + hy * p));
+			}
+		}
+		else {
+			return (dataType)(fmin(dx + hx * p, dy + hy * p));
+		}
+	}
+
+}
+
 void fastMarchingFrontPropagation2D(void* inputImageData, void* actionPtr, void* potentialPtr, void* endPoints, const PropagationType pType)
 {
 	switch (pType)
@@ -74,13 +155,13 @@ bool partialFrontPropagation2D(Image_Data2D inputImage, dataType* action, dataTy
 	//East
 	if (j < width - 1 && i >= 0 && i < length) {
 		size_t jplus = j + 1;
-		dataType x = selectX(action, length, width, i, jplus);
-		dataType y = selectY(action, length, width, i, jplus);
+		//dataType x = selectX(action, length, width, i, jplus);
+		//dataType y = selectY(action, length, width, i, jplus);
 		size_t indxEast = x_new(i, jplus, length);
 		dataType coefSpeed = potential[indxEast];
 		//dataType dEast = solve2dQuadratic(x, y, coefSpeed, spacing);
 		//pointFastMarching2D EastNeighbor = { i, jplus, dEast };
-		action[indxEast] = dEast;
+		//action[indxEast] = dEast;
 		//narrowBand.push_back(EastNeighbor);
 		labelArray[indxEast] = 2;
 	}
