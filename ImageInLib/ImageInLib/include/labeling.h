677f3f5a898a81a71c610ee46d775491b87dfa1b
Konan Allaly <konan.allaly@tatramed.sk>
2025-07-07 11:15:12 +0200

    add push element function prototype

--- ImageInLib/ImageInLib/include/labeling.h
+++ ImageInLib/ImageInLib/include/labeling.h
@@ -41,6 +41,8 @@ extern "C" {
 
 	void popFirstElement(labelingList* linked_list);
 
+	labelingPoint* pushElementToList(labelingList* linked_list, labelingPoint* linked_point, const dataType x, const dataType y, const dataType z);
+
 	bool labeling2D(Image_Data2D inputImageData, dataType* segment, dataType foreGroundValue);
 	
 	bool labeling3D(Image_Data inputImageData, dataType** segment, dataType foreGroundValue);
